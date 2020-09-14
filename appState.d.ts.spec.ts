0ebeb1135699097abd357bed95057e4b4cc147b5
István Pató <istvan.pato@gmail.com>
2020-09-14 14:45:07 +0200

    fix: prettier update, reformat

--- appState.d.ts.spec.ts
+++ appState.d.ts.spec.ts
@@ -2,12 +2,10 @@ import * as AppState from '.'
 import { AppStateInstance } from '.'
 
 // init application state handler with logging
-export const appStateInstance: AppStateInstance = AppState(
-  (appState: string, newAppState: string): void => {
-    if (appStateInstance.isError() || appStateInstance.isFatal()) {
-      console.error(`appstate ${appState} to ${newAppState}`)
-    } else {
-      console.warn(`appstate ${appState} to ${newAppState}`)
-    }
-  },
-)
+export const appStateInstance: AppStateInstance = AppState((appState: string, newAppState: string): void => {
+  if (appStateInstance.isError() || appStateInstance.isFatal()) {
+    console.error(`appstate ${appState} to ${newAppState}`)
+  } else {
+    console.warn(`appstate ${appState} to ${newAppState}`)
+  }
+})
